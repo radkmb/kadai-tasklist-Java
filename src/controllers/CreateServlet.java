4646adae22c8d9d32abfa587398871abe2beba43
radkmb <rad.kmb@gmail.com>
2018-06-24 08:08:05 +0900

    Add CreateServlet.java

---
+++ src/controllers/CreateServlet.java
@@ -0,0 +1,52 @@
+package controllers;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Task;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class CreateServlet
+ */
+@WebServlet("/create")
+public class CreateServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CreateServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String _token = (String)request.getParameter("_token");
+
+		if (_token != null && _token.equals(request.getSession().getId())) {
+		    EntityManager em = DBUtil.createEntityManager();
+
+		    Task t = new Task();
+
+		    String content = request.getParameter("content");
+		    t.setContent(content);
+
+		    em.getTransaction().begin();
+		    em.persist(t);
+		    em.getTransaction().commit();
+		    em.close();
+
+		    response.sendRedirect(request.getContextPath() + "/index");
+		}
+	}
+}
