48fccc8216fc53bf2bbe77a3b9c0adcac1f0ae77
radkmb <rad.kmb@gmail.com>
2018-06-24 08:48:56 +0900

    Add DestroyServlet.java

--- WebContent/WEB-INF/views/tasks/edit.jsp
+++ WebContent/WEB-INF/views/tasks/edit.jsp
@@ -8,6 +8,19 @@
             <c:import url="_form.jsp" />
         </form>
 
+        <p><a href="#" onclick="confirmDestroy();">このメッセージを削除する</a></p>
+        <form method="POST" action="${pageContext.request.contextPath}/destroy">
+            <input type="hidden" name="_token" value="${_token}" />
+        </form>
+
+        <script>
+            function confirmDestroy() {
+                if(confirm("本当に削除してよろしいですか？")) {
+                    document.forms[1].submit();
+                }
+            }
+        </script>
+
         <p><a href="${pageContext.request.contextPath}/index">タスク一覧に戻る</a></p>
 
     </c:param>
---
+++ src/controllers/DestroyServlet.java
@@ -0,0 +1,51 @@
+package controllers;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Task;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class DestroyServlet
+ */
+@WebServlet("/destroy")
+public class DestroyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DestroyServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    String _token = (String)request.getParameter("_token");
+
+	    if (_token != null && _token.equals(request.getSession().getId())) {
+	        EntityManager em = DBUtil.createEntityManager();
+
+	        Task t = em.find(Task.class, (Integer)(request.getSession().getAttribute("task_id")));
+
+	        em.getTransaction().begin();
+	        em.remove(t);
+	        em.getTransaction().commit();
+	        em.close();
+
+	        request.getSession().removeAttribute("task_id");
+
+	        response.sendRedirect(request.getContextPath() + "/index");
+	    }
+	}
+}
