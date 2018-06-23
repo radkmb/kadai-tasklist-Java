e87fdf8d47da064bc110d9f9f8eee90e939302f1
radkmb <rad.kmb@gmail.com>
2018-06-24 07:02:42 +0900

    Add getAllTasks

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -1,11 +1,17 @@
 package controllers;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.Task;
+import utils.DBUtil;
 
 /**
  * Servlet implementation class IndexServlet
@@ -13,7 +19,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/index")
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -26,8 +32,12 @@ public class IndexServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+	    EntityManager em = DBUtil.createEntityManager();
 
+	    List<Task> tasks = em.createNamedQuery("getAllTasks", Task.class)
+	                               .getResultList();
+	    response.getWriter().append(Integer.valueOf(tasks.size()).toString());
+
+	    em.close();
+	}
 }
