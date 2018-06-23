0a604cfa3c812b78bb29fbef5ae89ceb6155e4b3
radkmb <rad.kmb@gmail.com>
2018-06-24 08:39:41 +0900

    Add UpdateServlet.java

---
+++ src/controllers/UpdateServlet.java
@@ -0,0 +1,53 @@
+package controllers;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Task;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class UpdateServlet
+ */
+@WebServlet("/update")
+public class UpdateServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UpdateServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    String _token = (String)request.getParameter("_token");
+
+	    if(_token != null && _token.equals(request.getSession().getId())) {
+            EntityManager em = DBUtil.createEntityManager();
+
+            Task t = em.find(Task.class, (Integer)(request.getSession().getAttribute("task_id")));
+
+            String content = request.getParameter("content");
+            t.setContent(content);
+
+            em.getTransaction().begin();
+            em.getTransaction().commit();
+            em.close();
+
+            request.getSession().removeAttribute("task_id");
+
+            response.sendRedirect(request.getContextPath() + "/index");
+	    }
+	}
+}
